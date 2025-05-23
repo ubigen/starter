0e9af32b1af42beff9fe41b8028e88ff956f68a9
civiumit <umitcivi@gmail.com>
2025-05-23 11:11:06 +0300

    Refactor program structure and enhance API functionality.
    Streamlined service registration by introducing modular extensions for commands, queries, validators, and endpoints. Added a custom exception handler and removed unused weather forecast code to simplify the application and improve maintainability.

--- Starter.Api/Program.cs
+++ Starter.Api/Program.cs
@@ -1,41 +1,34 @@
+using Scalar.AspNetCore;
+using Starter.Api.Extensions.Command;
+using Starter.Api.Extensions.Endpoint;
+using Starter.Api.Extensions.Query;
+using Starter.Api.Extensions.Validator;
+using Starter.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+    .AddOpenApi()
+    .AddValidators()
+    .AddCommand()
+    .AddQuery()
+    .AddEndpoints()
+    .AddExceptionHandler<ExceptionHandler>();
+
 
 var app = builder.Build();
+
+app.UseExceptionHandler(_ => { });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
+app.MapEndpoints();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
